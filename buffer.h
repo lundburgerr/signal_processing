2a8008578c407ccd1e57fa8e46255c3e9fd4fecc
Robin Lundberg <lundburgerr@gmail.com>
2023-09-07 08:01:21 -0400

    Added double buffer functionality for complex values (#4)

--- buffer.h
+++ buffer.h
@@ -7,12 +7,29 @@ typedef struct DoubleBuffer {
   int oldest;  // Index to the oldest element.
 } DoubleBuffer;
 
+typedef struct DoubleBufferComplex {
+  float* buffer_real;  // Size is 2*size.
+  float* buffer_imag;  // Size is 2*size.
+  const int size;
+  int oldest;  // Index to the oldest element.
+} DoubleBufferComplex;
+
 // Creates a double buffer and sets all buffer values to zero.
 inline DoubleBuffer double_buffer_init(float* buffer, int size) {
   for (int i = 0; i < 2 * size; ++i) {
     buffer[i] = 0;
   }
   return {buffer, size, /*oldest*/ 0};
+}
+
+inline DoubleBufferComplex double_buffer_complex_init(float* buffer_real,
+                                                      float* buffer_imag,
+                                                      int size) {
+  for (int i = 0; i < 2 * size; ++i) {
+    buffer_real[i] = 0;
+    buffer_imag[i] = 0;
+  }
+  return {buffer_real, buffer_imag, size, /*oldest*/ 0};
 }
 
 // Update the buffer with the new input.
@@ -28,6 +45,24 @@ inline void double_buffer_update(const float* x, int in_size,
   }
   for (int i = 0; i < in_size; ++i) {
     buffer[buffer_size + start + i] = x[i];
+  }
+  double_buffer->oldest = (start + in_size) % buffer_size;
+}
+
+inline void double_buffer_complex_update(const float* x_real,
+                                         const float* x_imag, int in_size,
+                                         DoubleBufferComplex* double_buffer) {
+  int buffer_size = double_buffer->size;
+  int start = double_buffer->oldest;
+  float* buffer_real = double_buffer->buffer_real;
+  float* buffer_imag = double_buffer->buffer_imag;
+  for (int i = 0; i < in_size; ++i) {
+    buffer_real[start + i] = x_real[i];
+    buffer_imag[start + i] = x_imag[i];
+  }
+  for (int i = 0; i < in_size; ++i) {
+    buffer_real[buffer_size + start + i] = x_real[i];
+    buffer_imag[buffer_size + start + i] = x_imag[i];
   }
   double_buffer->oldest = (start + in_size) % buffer_size;
 }
--- buffer_test.cpp
+++ buffer_test.cpp
@@ -23,6 +23,20 @@ TEST(DoubleBufferTest, Init) {
   EXPECT_THAT(expected, ElementsAreArray(double_buffer.buffer, 10));
 }
 
+TEST(DoubleBufferComplexTest, Init) {
+  int size = 5;
+  std::vector<float> buffer_real(2 * size, 1.0f);
+  std::vector<float> buffer_imag(2 * size, 1.0f);
+  DoubleBufferComplex double_buffer =
+      double_buffer_complex_init(buffer_real.data(), buffer_imag.data(), size);
+  EXPECT_EQ(double_buffer.size, size);
+  EXPECT_EQ(double_buffer.oldest, 0);
+
+  std::vector<float> expected(10, 0.0f);
+  EXPECT_THAT(expected, ElementsAreArray(double_buffer.buffer_real, 10));
+  EXPECT_THAT(expected, ElementsAreArray(double_buffer.buffer_imag, 10));
+}
+
 TEST(DoubleBufferTest, Update) {
   int size = 5;
   std::vector<float> buffer(2 * size, 1.0f);
@@ -37,4 +51,27 @@ TEST(DoubleBufferTest, Update) {
                                  1.0, 2.0, 3.0, 0.0, 0.0};
   EXPECT_THAT(expected, ElementsAreArray(double_buffer.buffer, 10));
 }
+
+TEST(DoubleBufferComplexTest, Update) {
+  int size = 5;
+  std::vector<float> buffer_real(2 * size, 1.0f);
+  std::vector<float> buffer_imag(2 * size, 1.0f);
+  DoubleBufferComplex double_buffer =
+      double_buffer_complex_init(buffer_real.data(), buffer_imag.data(), size);
+
+  std::vector<float> x_real = {1.0, 2.0, 3.0};
+  std::vector<float> x_imag = {-1.0, -2.0, -3.0};
+  double_buffer_complex_update(x_real.data(), x_imag.data(), /*in_size=*/3,
+                               &double_buffer);
+  EXPECT_EQ(double_buffer.size, size);
+  EXPECT_EQ(double_buffer.oldest, 3);
+
+  std::vector<float> expected_real = {1.0, 2.0, 3.0, 0.0, 0.0,
+                                      1.0, 2.0, 3.0, 0.0, 0.0};
+  std::vector<float> expected_imag = {-1.0, -2.0, -3.0, 0.0, 0.0,
+                                      -1.0, -2.0, -3.0, 0.0, 0.0};
+  EXPECT_THAT(expected_real, ElementsAreArray(double_buffer.buffer_real, 10));
+  EXPECT_THAT(expected_imag, ElementsAreArray(double_buffer.buffer_imag, 10));
+}
+
 }  // namespace
