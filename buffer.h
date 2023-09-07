f45b34adf046ccac584911ec5d8314bec3a71289
Robin Lundberg <lundburgerr@gmail.com>
2023-09-07 20:33:30 +0200

    Added double buffer for interleaved complex values

--- buffer.h
+++ buffer.h
@@ -14,14 +14,26 @@ typedef struct DoubleBufferComplex {
   int oldest;  // Index to the oldest element.
 } DoubleBufferComplex;
 
+// Double buffer with complex values where odd indices are real and even indices
+// are imaginary.
+typedef struct DoubleBufferComplexInterleaved {
+  float* buffer_interleaved;  // Size is 4*size.
+  const int size;
+  int oldest;  // Index to the oldest element.
+} DoubleBufferComplexInterleaved;
+
+// Input buffer has length 2*size.
 // Creates a double buffer and sets all buffer values to zero.
 inline DoubleBuffer double_buffer_init(float* buffer, int size) {
   for (int i = 0; i < 2 * size; ++i) {
     buffer[i] = 0;
-  }
+  }  // Input buffer has length 2*size.
+     // Creates a double buffer and sets all buffer values to zero.
   return {buffer, size, /*oldest*/ 0};
 }
 
+// Input buffers has length 2*size.
+// Creates a double buffer and sets all buffer values to zero.
 inline DoubleBufferComplex double_buffer_complex_init(float* buffer_real,
                                                       float* buffer_imag,
                                                       int size) {
@@ -30,6 +42,16 @@ inline DoubleBufferComplex double_buffer_complex_init(float* buffer_real,
     buffer_imag[i] = 0;
   }
   return {buffer_real, buffer_imag, size, /*oldest*/ 0};
+}
+
+// Input buffer has length 4*size.
+// Creates a double buffer and sets all buffer values to zero.
+inline DoubleBufferComplexInterleaved double_buffer_complex_interleaved_init(
+    float* buffer_interleaved, int size) {
+  for (int i = 0; i < 4 * size; ++i) {
+    buffer_interleaved[i] = 0;
+  }
+  return {buffer_interleaved, size, /*oldest*/ 0};
 }
 
 // Update the buffer with the new input.
@@ -65,6 +87,23 @@ inline void double_buffer_complex_update(const float* x_real,
     buffer_imag[buffer_size + start + i] = x_imag[i];
   }
   double_buffer->oldest = (start + in_size) % buffer_size;
+}
+
+inline void double_buffer_complex_interleaved_update(
+    const float* x_real, const float* x_imag, int in_size,
+    DoubleBufferComplexInterleaved* double_buffer) {
+  int buffer_size = double_buffer->size;
+  int start = double_buffer->oldest;
+  float* buffer_interleaved = double_buffer->buffer_interleaved;
+  for (int i = 0; i < in_size; ++i) {
+    buffer_interleaved[2 * i + start] = x_real[i];
+    buffer_interleaved[2 * i + start + 1] = x_imag[i];
+  }
+  for (int i = 0; i < in_size; ++i) {
+    buffer_interleaved[2 * buffer_size + 2 * i + start] = x_real[i];
+    buffer_interleaved[2 * buffer_size + 2 * i + start + 1] = x_imag[i];
+  }
+  double_buffer->oldest = (start + 2 * in_size) % (2 * buffer_size);
 }
 
 #endif  // SIGNAL_PROCESSING__BUFFER_H_
